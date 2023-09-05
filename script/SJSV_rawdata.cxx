b61e67cae890321ec153f2db69ac58940cab3ece
Shihai Jia <shihai.jia@outlook.com>
2023-09-05 10:31:56 +0200

    Optimised example script

--- script/SJSV_rawdata.cxx
+++ script/SJSV_rawdata.cxx
@@ -1,11 +1,8 @@
 #include <iostream>
-
+#include "TCanvas.h" 
 #include "easylogging++.h"
 #include "SJSV_pcapreader.h"
 #include "SJSV_eventbuilder.h"
-
-#include "TCanvas.h" 
-
 
 void set_easylogger(); // set easylogging++ configurations
 
@@ -15,124 +12,103 @@ int main(int argc, char** argv) {
     LOG(INFO) << "Start SJSV_rawdata.cxx";
 
     std::string filename_pcap = "../data/traffic_2023082102.pcap";
+    auto AOI_Start      =  10'000'000;
+    auto AOI_End        = 200'000'000;
+    auto qp_channel_vec_adc  = std::vector<uint16_t>{64, 65, 66, 67, 68, 69, 70, 71};
+    Int_t bin_num       = 50;
+    Double_t bin_low    = 200;
+    Double_t bin_high   = 350;
+    auto qp_channel_vec_hist = std::vector<uint16_t>{74, 75, 76, 77, 78, 79};
+    auto color_list     = std::vector<Color_t>{kRed, kBlue, kGreen, kBlack, kOrange, kMagenta};
 
     // split filename according to '_'
     std::string filename_raw_root = "../tmp/raw_" + filename_pcap.substr(filename_pcap.find_last_of("_")+1, filename_pcap.find_last_of(".")-filename_pcap.find_last_of("_")-1) + ".root";
 
     std::string filename_parsed_root = "../tmp/parsed_" + filename_pcap.substr(filename_pcap.find_last_of("_")+1, filename_pcap.find_last_of(".")-filename_pcap.find_last_of("_")-1) + ".root";
 
-    // * Test pcapreader
+    // ! Create PCAP reader and read PCAP file into raw rootfile
+    // * -------------------------------------------------------------------------------------------
     SJSV_pcapreader pcapreader(filename_pcap);
-    // * Run a single frame test
-    // std::vector<uint8_t> frame = std::vector<uint8_t>{0x53, 0x70, 0x21, 0xdf, 0xd8, 0x62};
-    // auto res_str = pcapreader.test_single_frame_decode(frame);
-    // LOG(INFO) << "Single frame decode result: " << res_str;
-    auto AOI_Start = 100'000'000;
-    auto AOI_End   = 200'000'000;
-
     pcapreader.read_pcapfile();
     pcapreader.test_decode_first_packet();
     auto vec_len = pcapreader.full_decode_pcapfile();
+    LOG(INFO) << "Saving to raw rootfile ...";
     if (pcapreader.save_to_rootfile(filename_raw_root))
         LOG(INFO) << "Save to rootfile success";
     else
         LOG(ERROR) << "Save to rootfile fail";
+    // * -------------------------------------------------------------------------------------------
 
-    // * Test eventbuilder
+    // ! Create event builder and read raw rootfile into parsed rootfile
+    // * -------------------------------------------------------------------------------------------
     SJSV_eventbuilder eventbuilder;
     eventbuilder.load_raw_data(filename_raw_root);
     eventbuilder.set_bcid_cycle(25);
     eventbuilder.set_tdc_slope(60);
     eventbuilder.parse_raw_data();
+    LOG(INFO) << "Saving to parsed rootfile ...";
     if (eventbuilder.save_parsed_data(filename_parsed_root))
         LOG(INFO) << "Save to rootfile success";
     else
         LOG(ERROR) << "Save to rootfile fail";
+    // * -------------------------------------------------------------------------------------------
 
+    // * -- Plot reconstructed time --
     auto _temp_pedestal = eventbuilder.load_pedestal_csv("../data/config/Pedestal_153653.csv");
-
     auto _temp_simple_pedestal = eventbuilder.get_simple_pedestal();
     eventbuilder.update_pedestal(_temp_simple_pedestal);
     // eventbuilder.enable_pedestal_subtraction(true);
 
-    auto qb_canvas2 = new TCanvas("qb_canvas2", "Quick browse2", 1200, 1000);
+    auto qb_canvas_time_index = new TCanvas("qb_canvas2", "Quick browse2", 1200, 1000);
     auto qb_tgraph2 = eventbuilder.quick_plot_time_index(AOI_Start, AOI_End);
     qb_tgraph2->Draw("APL");
-    // add grid
-    qb_canvas2->SetGrid();
-    qb_canvas2->SaveAs("../pics/quick_browse_time.png");
-    qb_canvas2->Close();
+ 
+    qb_canvas_time_index->SetGrid();
+    qb_canvas_time_index->SaveAs("../pics/quick_browse_time.png");
+    qb_canvas_time_index->Close();
 
-    auto qb_canvas = new TCanvas("qb_canvas", "Quick browse", 1200, 1000);
-    // set title
-    // auto qb_tgraph = eventbuilder.quick_plot_single_channel(4, 50000000, 400000000);
-    // auto qb_tgraph = eventbuilder.quick_plot_time_index(0, 700'000'000);
-    auto qp_channel_vec = std::vector<uint16_t>{64, 65, 66, 67, 68, 69, 70, 71};
-    auto qb_tgraph = eventbuilder.quick_plot_multiple_channels(qp_channel_vec, AOI_Start, AOI_End);
+    // * -- Plot multichannel ADC --
+    auto qb_canvas_multi_ADC = new TCanvas("qb_canvas", "Quick browse", 1200, 1000);
+
+    auto qb_tgraph = eventbuilder.quick_plot_multiple_channels(qp_channel_vec_adc, AOI_Start, AOI_End);
     qb_tgraph->Draw("APL");
     auto _legend = new TLegend(0.7, 0.7, 0.9, 0.9);
-    for (auto i = 0; i < qp_channel_vec.size(); i++){
-        _legend->AddEntry(qb_tgraph->GetListOfGraphs()->At(i), ("Channel " + std::to_string(qp_channel_vec[i])).c_str(), "l");
-    }
+    for (auto i = 0; i < qp_channel_vec_adc.size(); i++)
+        _legend->AddEntry(qb_tgraph->GetListOfGraphs()->At(i), ("Channel " + std::to_string(qp_channel_vec_adc[i])).c_str(), "l");
     _legend->Draw();
-    qb_canvas->SetGrid();
-    // set transparent background
-    qb_canvas->SaveAs("../pics/quick_browse_multichn.png");
-    qb_canvas->Close();
+    qb_canvas_multi_ADC->SetGrid();
+    qb_canvas_multi_ADC->SaveAs("../pics/quick_browse_multichn.png");
+    qb_canvas_multi_ADC->Close();
 
+    // * -- Plot reconstructed ADC histogram --
+    auto qp_canvas_multi_ADC_hist = new TCanvas("qp_canvas", "Quick plot", 1200, 1000);
 
-    Int_t bin_num = 50;
-    Double_t bin_low = 200;
-    Double_t bin_high = 350;
-    auto qp_canvas = new TCanvas("qp_canvas", "Quick plot", 1200, 1000);
-    auto qp_hist1 = eventbuilder.quick_plot_single_channel_hist(74, bin_num, bin_low, bin_high);
-    auto qp_hist2 = eventbuilder.quick_plot_single_channel_hist(75, bin_num, bin_low, bin_high);
-    auto qp_hist3 = eventbuilder.quick_plot_single_channel_hist(76, bin_num, bin_low, bin_high);
-    auto qp_hist4 = eventbuilder.quick_plot_single_channel_hist(77, bin_num, bin_low, bin_high);
-    auto qp_hist5 = eventbuilder.quick_plot_single_channel_hist(78, bin_num, bin_low, bin_high);
-    auto qp_hist6 = eventbuilder.quick_plot_single_channel_hist(79, bin_num, bin_low, bin_high);
+    std::vector<TH1D*> _vec_hist;
+    for (auto _channel : qp_channel_vec_hist) {
+        auto _hist = eventbuilder.quick_plot_single_channel_hist(_channel, bin_num, bin_low, bin_high);
+        _vec_hist.push_back(_hist);
+    }
 
-    qp_hist1->SetLineColor(kRed);
-    qp_hist2->SetLineColor(kBlue);
-    qp_hist3->SetLineColor(kGreen);
-    qp_hist4->SetLineColor(kBlack);
-    qp_hist5->SetLineColor(kOrange);
-    qp_hist6->SetLineColor(kMagenta);
-
-    qp_hist1->SetLineWidth(2);
-    qp_hist2->SetLineWidth(2);
-    qp_hist3->SetLineWidth(2);
-    qp_hist4->SetLineWidth(2);
-    qp_hist5->SetLineWidth(2);
-    qp_hist6->SetLineWidth(2);
-
-    qp_hist1->SetStats(0);
-    qp_hist2->SetStats(0);
-    qp_hist3->SetStats(0);
-    qp_hist4->SetStats(0);
-    qp_hist5->SetStats(0);
-    qp_hist6->SetStats(0);
-
-    qp_hist1->Draw("");
-    qp_hist2->Draw("same");
-    qp_hist3->Draw("same");
-    qp_hist4->Draw("same");
-    qp_hist5->Draw("same");
+    for (auto i = 0; i < qp_channel_vec_hist.size(); i++){
+        _vec_hist.at(i)->SetLineColor(color_list.at(i));
+        _vec_hist.at(i)->SetLineWidth(2);
+        _vec_hist.at(i)->SetStats(0);
+        _vec_hist.at(i)->GetXaxis()->SetTitle("ADC");
+        _vec_hist.at(i)->GetYaxis()->SetTitle("Counts");
+        _vec_hist.at(i)->Draw("same");
+    }
 
     auto _legend2 = new TLegend(0.7, 0.7, 0.9, 0.9);
-    _legend2->AddEntry(qp_hist1, "Channel 74", "l");
-    _legend2->AddEntry(qp_hist2, "Channel 75", "l");
-    _legend2->AddEntry(qp_hist3, "Channel 76", "l");
-    _legend2->AddEntry(qp_hist4, "Channel 77", "l");
-    _legend2->AddEntry(qp_hist5, "Channel 78", "l");
-    _legend2->AddEntry(qp_hist6, "Channel 79", "l");
+    for (auto i = 0; i < qp_channel_vec_hist.size(); i++)
+        _legend2->AddEntry(_vec_hist.at(i), ("Channel " + std::to_string(qp_channel_vec_hist[i])).c_str(), "l");
     _legend2->Draw();
 
-    // Set log scale
-    qp_canvas->SetLogy();
+    qp_canvas_multi_ADC_hist->SetLogy();
+    qp_canvas_multi_ADC_hist->SaveAs("../pics/quick_plot_hist.png");
+    qp_canvas_multi_ADC_hist->Close();
 
-    qp_canvas->SaveAs("../pics/quick_plot_hist.png");
-    qp_canvas->Close();
+    for (auto _hist : _vec_hist)
+        delete _hist;
 
     return 0;
 }
